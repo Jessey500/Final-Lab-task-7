0eddce10a098b9a5056346930712cae8e771a0d7
Jesey500 <Jess.jesey@gmail.com>
2020-11-25 13:50:18 +0600

    Test Interface 1

---
+++ Test  Interface  1/BasicCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test__Interface__1
+{
+    class BasicCalculator : BasicCalculatorInterface 
+    {
+       
+        int r;
+        public int sum(int n1, int n2)
+        {
+            r = n1 + n2;
+            return r;
+        }
+        public int sub(int n1, int n2)
+        {
+            r = n1 - n2;
+            return r;
+        }
+        public int multiplication(int n1, int n2)
+        {
+            r = n1 * n2;
+            return r;
+        }
+        public int division(int n1, int n2)
+        {
+            r = n1 / n2;
+            return r;
+        }
+        public void ShowResult()
+        {
+            Console.WriteLine("result  : " + r);
+        }
+    }
+}
---
+++ Test  Interface  1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test__Interface__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Calculator Interface");
+            Console.WriteLine();
+            BasicCalculator c = new BasicCalculator();
+            Console.WriteLine("Summation is :");
+            Console.WriteLine();
+            c.sum(8, 7);
+            c.ShowResult();
+            Console.WriteLine("Subtraction is :");
+            Console.WriteLine();
+            c.sub(8, 7);
+            c.ShowResult();
+            Console.WriteLine("Multiplication is :");
+            Console.WriteLine();
+            c.multiplication(8, 7);
+            c.ShowResult();
+            Console.WriteLine("Division is :");
+            Console.WriteLine();
+            c.division(8, 7);
+            c.ShowResult();
+        }
+    }
+}
